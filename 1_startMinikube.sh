3ff0ccee1ea8da11fe3516db70c51d2fe2e59f65
Burr Sutter <burrsutter@gmail.com>
2019-02-18 09:54:30 -0500

    adding new renamed

---
+++ 1_startMinikube.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+minikube config set memory 10240
+minikube config set cpus 4
+minikube config set vm-driver virtualbox #hyperkit, kvm2
+minikube config set disk-size 30g
+minikube start --kubernetes-version v1.11.5
+#minikube start 
+
+# minikube addons enable ingress
+# minikube addons enable heapster

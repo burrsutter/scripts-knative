314f621bcfebfbea70f8d6e39c61d43f5606fa4d
Burr Sutter <burrsutter@gmail.com>
2019-03-04 12:59:25 -0500

    trying hyperkit

--- 1_startMinikube.sh
+++ 1_startMinikube.sh
@@ -2,6 +2,6 @@
 
 minikube start -p knative --memory=8192 --cpus=4 \
   --kubernetes-version=v1.12.0 \
-  --vm-driver=virtualbox \
+  --vm-driver=hyperkit \ 
   --disk-size=50g \
   --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"

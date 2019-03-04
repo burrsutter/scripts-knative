42d2b561cb35a408dfad3c93fc944d2c545274a7
Burr Sutter <burrsutter@gmail.com>
2019-03-04 12:22:39 -0500

    matching tutorial

--- 1_startMinikube.sh
+++ 1_startMinikube.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-minikube config set memory 10240
-minikube config set cpus 4
-minikube config set vm-driver virtualbox #hyperkit, kvm2
-minikube config set disk-size 50g
-minikube start --profile knative --kubernetes-version v1.12.0  --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
-
+minikube start -p knative --memory=8192 --cpus=4 \
+  --kubernetes-version=v1.12.0 \
+  --vm-driver=virtualbox \
+  --disk-size=50g \
+  --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"

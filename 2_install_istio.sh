229f7525da6dad7a3db5572c5f7468a72ff0fc93
Burr Sutter <burrsutter@gmail.com>
2019-03-07 23:23:47 -0500

    lots of refactoring, renaming

--- 2_install_istio.sh
+++ 2_install_istio.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-curl -L https://github.com/istio/istio/releases/download/1.0.5/istio-1.0.5-osx.tar.gz | tar xz
+# curl -L https://github.com/istio/istio/releases/download/1.0.5/istio-1.0.5-osx.tar.gz | tar xz
 
 # curl -L https://github.com/istio/istio/releases/download/1.0.5/istio-1.0.5-linux.tar.gz | tar xz
 
-cd istio-1.0.5
+# cd istio-1.0.5
 
-kubectl apply -f install/kubernetes/helm/istio/templates/crds.yaml
+# kubectl apply -f install/kubernetes/helm/istio/templates/crds.yaml
 
-kubectl apply -f install/kubernetes/istio-demo.yaml
+# kubectl apply -f install/kubernetes/istio-demo.yaml
 
-cd ..
+# cd ..
+
+
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.3.0/istio-crds.yaml
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.3.0/istio.yaml
 
 
 
--- 5_open_kiali.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-open http://$(minikube -p knative ip):$(kubectl get svc kiali -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
-
-echo "admin/admin"

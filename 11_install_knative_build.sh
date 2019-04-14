71f0e56e867e964a9fdbeb978febadf2b08ec5b4
Burr Sutter <burrsutter@gmail.com>
2019-04-14 18:19:32 -0400

    upgrading to knative 0.4

--- 11_install_knative_build.sh
+++ 11_install_knative_build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-kubectl apply --filename https://github.com/knative/build/releases/download/v0.3.0/release.yaml
+# kubectl apply --filename https://github.com/knative/build/releases/download/v0.3.0/release.yaml
+kubectl apply --filename https://github.com/knative/build/releases/download/v0.4.0/build.yaml
--- 12_install_knative_eventing.sh
+++ 12_install_knative_eventing.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.3.0/release.yaml
-kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.3.0/in-memory-channel.yaml
-kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/v0.3.0/release.yaml
+# kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.3.0/release.yaml
+# kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.3.0/in-memory-channel.yaml
+# kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/v0.3.0/release.yaml
+
+kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.4.0/release.yaml && \
+kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/v0.4.0/release.yaml && \
+kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.4.0/third_party/config/build/clusterrole.yaml

9323b585cc2d31061c2632c92e9ba630843d3f5e
Burr Sutter <burrsutter@gmail.com>
2019-04-14 17:08:14 -0400

    updating env vars

--- 0_setenv.sh
+++ 0_setenv.sh
@@ -2,17 +2,11 @@
 
 echo 'source 0_setenv.sh'
 
-export MINIKUBE_HOME=/Users/burrsutter/minikube_0.35.0/bin;
+export MINIKUBE_HOME=/Users/burrsutter/minikube_1.0.0/bin;
 export PATH=$MINIKUBE_HOME:$PATH
 # export KUBECONFIG=$MINIKUBE_HOME/.kube/config:$MINIKUBE_HOME/.kube/kubconfig2
 export KUBECONFIG=$MINIKUBE_HOME/.kube/config
 export KUBE_EDITOR="code -w"
-
-export ISTIO_HOME=/Users/burrsutter/minikube_0.35.0/istio-1.0.5
-export PATH=$ISTIO_HOME/bin:$PATH
-
-# https://github.com/GoogleContainerTools/krew
-# export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 echo 'the following after minikube as started'
 echo 'eval $(minikube --profile knative docker-env)'

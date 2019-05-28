5346772628d617f8447c8328da4da2f79914abd7
Burr Sutter <burrsutter@gmail.com>
2019-05-28 17:24:12 +0200

    upated

--- 3_install_knative_serving.sh
+++ 3_install_knative_serving.sh
@@ -9,3 +9,4 @@
 # kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/serving.yaml
 
 kubectl apply --filename https://github.com/knative/serving/releases/download/v0.5.2/serving.yaml
+

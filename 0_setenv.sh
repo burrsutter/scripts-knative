6ddbddf8294850b4e85f44dbd52fb66ee2178960
Burr Sutter <burrsutter@gmail.com>
2019-06-20 08:16:45 -0400

    updating to minikube 1.1.1

--- 0_setenv.sh
+++ 0_setenv.sh
@@ -2,12 +2,11 @@
 
 echo 'source 0_setenv.sh'
 
-export MINIKUBE_HOME=/Users/burrsutter/minikube_1.0.1;
+export MINIKUBE_HOME=/Users/burrsutter/minikube_1.1.1;
 export PATH=$MINIKUBE_HOME/bin:$PATH
 export KUBECONFIG=$MINIKUBE_HOME/.kube/config:$MINIKUBE_HOME/.kube/kubconfig2
-export KUBECONFIG=$MINIKUBE_HOME/.kube/config
 export KUBE_EDITOR="code -w"
 
-echo 'the following after minikube as started'
+echo 'the following AFTER minikube has started'
 echo 'eval $(minikube --profile knative docker-env)'
 

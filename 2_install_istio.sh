6835c18ffd7bcc5deff6430181c76bd170dc6718
Burr Sutter <burrsutter@gmail.com>
2019-08-13 17:56:42 -0400

    updating knative to 0.7.1

--- 2_install_istio.sh
+++ 2_install_istio.sh
@@ -6,9 +6,10 @@
 # kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio-crds.yaml && \
 # kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio.yaml
 
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.5.2/istio-crds.yaml && \
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.5.2/istio.yaml
+# kubectl apply --filename https://github.com/knative/serving/releases/download/v0.5.2/istio-crds.yaml && \
+# kubectl apply --filename https://github.com/knative/serving/releases/download/v0.5.2/istio.yaml
 
-
+kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.7.1/third_party/istio-1.1.7/istio-crds.yaml && \
+kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.7.1/third_party/istio-1.1.7/istio.yaml
 
 
--- 5_install_knative_eventing.sh
+++ 5_install_knative_eventing.sh
@@ -8,6 +8,9 @@
 # kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/v0.4.0/release.yaml && \
 # kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.4.0/third_party/config/build/clusterrole.yaml
 
-kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.5.0/release.yaml && \
-kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/v0.5.0/eventing-sources.yaml && \
-kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.5.2/third_party/config/build/clusterrole.yaml
+# kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.5.0/release.yaml && \
+# kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/v0.5.0/eventing-sources.yaml && \
+# kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.5.2/third_party/config/build/clusterrole.yaml
+
+kubectl apply --selector networking.knative.dev/certificate-provider!=cert-manager \
+  --filename https://github.com/knative/eventing/releases/download/v0.7.1/release.yaml

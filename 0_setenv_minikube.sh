0195edbba1d310812511b172d5eb49da63277350
Burr Sutter <burrsutter@gmail.com>
2019-10-06 17:24:56 -0400

    renaming setenv script

--- 0_setenv_minikube.sh
+++ 0_setenv_minikube.sh
@@ -2,9 +2,9 @@
 
 echo 'source 0_setenv.sh'
 
-export MINIKUBE_HOME=/Users/burrsutter/minikube_1.2.0;
+export MINIKUBE_HOME=/Users/burrsutter/minikube;
 export PATH=$MINIKUBE_HOME/bin:$PATH
-export KUBECONFIG=$MINIKUBE_HOME/.kube/config
+export KUBECONFIG=$MINIKUBE_HOME/knative/.kube/config
 export KUBE_EDITOR="code -w"
 
 echo 'the following AFTER minikube has started'
